6592c46a7a1c8ce0ab623865114022380a15d382
Martin Thoma <info@martin-thoma.de>
2012-11-12 12:45:27 +0100

    neue Folien hochgeladen;neue Javadoc für Blatt 1

--- Tutorium-04/SimpleLoop.java
+++ Tutorium-04/SimpleLoop.java
@@ -1,8 +1,8 @@
 public class SimpleLoop {
-	public static void main(String[] args) {
-		for (int i = -5; i < 15; ++i) {
-			System.out.println(i + ": " + i*i);
-		}
+    public static void main(String[] args) {
+        for (int i = -5; i < 15; ++i) {
+            System.out.println(i + ": " + i * i);
+        }
 
-	}
+    }
 }
